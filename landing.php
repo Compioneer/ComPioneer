b696a3f084ff1bb87e941411c12cf465e709ea15
1Noura71 <1norraa.s7@gmail.com>
2020-11-12 23:16:18 +0300

    multiple updates on landing page

--- landing.php
+++ landing.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Landing</title>
-    <link rel="stylesheet"  href="css/style.css">
-    <link rel="stylesheet"  href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  </head>
-  <body>
+<?php
+  require 'include\header.php';
+?>
+
 
             <div class="container">
             <div class="login-box">
