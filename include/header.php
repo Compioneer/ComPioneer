51786f368ade1ca900d0df2a06829ee24a046cd8
1Noura71 <1norraa.s7@gmail.com>
2020-11-12 23:14:17 +0300

    include header

---
+++ include/header.php
@@ -0,0 +1,12 @@
+<?php
+  require 'db.php';
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Landing</title>
+    <link rel="stylesheet"  href="../css/style.css">
+    <link rel="stylesheet"  href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  </head>
+  <body>
