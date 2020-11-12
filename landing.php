d860fca31c617e96d6334f4d9de3e7465c83a51b
1noura71 <1norraa.s7@gmail.com>
2020-11-12 16:09:24 +0300

    update link element "css"

--- landing.php
+++ landing.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>Landing</title>
-    <link rel="stylesheet"  href="style.css">
+    <link rel="stylesheet"  href="css/style.css">
     <link rel="stylesheet"  href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   </head>
   <body>
