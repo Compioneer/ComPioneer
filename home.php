cda6e316a351df4ccd91d7184636f38924508d79
razanas <razan8022@gmil.com>
2020-11-15 00:55:33 +0300

    link a button to postQuestion page

--- home.php
+++ home.php
@@ -3,6 +3,7 @@ session_start();
 if(!isset($_SESSION['username'])){
   header('location:landing.php?Signing=Failed');
 }
+require 'include\header.php';
  ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -19,7 +20,9 @@ if(!isset($_SESSION['username'])){
 
       <h1>Welcome <?php echo $_SESSION['username']; ?> </h1>
       <p>hello 9</p>
-
+<a href="post_question.php">
+<button type="button" class="btn-class" id="btn-id">+</button>
+</a>
 
 
   </body>
