4d322e05558acd79559f2d8ebdf5ee2eeb4dcd32
ghada-shl <ghada11pc@gmail.com>
2020-11-14 22:48:03 +0300

    landing page styling

--- landing.php
+++ landing.php
@@ -2,52 +2,38 @@
   require 'include\header.php';
 ?>
 
+<div class="header">
+  <a href="#default" class="logo">
+    ComPioneer
+  </a>
 
-            <div class="container">
-            <div class="login-box">
-              <div class="row">
-                <div class="col-md-6 login-left">
-                  <h2>Login</h2>
-                  <form action="login.php" method="post">
-                    <div class="form-group">
+  <div class="container">
+        <div class="row">
+              <div class="col-md-7 mb-3">
+                  <img src="../images/illu.svg" class="img-fluid" alt="illustration">
+              </div>
+
+                 <div class="col-md-4">
+                   <h4 class="signin-text mb-3"> Ask a Question,</br> Get an Answer</h4>
+                   <h3> </br> </h3>
+                   <form action="login.php" method="post">
+                     <div class="form-group">
                       <label>Username</label>
                       <input type="text" name="user_or_email" class="form-control" required>
-                    </div>
+                     </div>
                     <div class="form-group">
                       <label>Password</label>
                       <input type="password" name="password" class="form-control" required>
                     </div>
-                    <button type="submit" name = "submit" class="btn btn-primary">Login</button>
-                  </form>
-                </div>
-
-
-
-                <div class="col-md-6 login-right">
-                  <h2>Sign Up</h2>
-                  <form action="signup.php" method="post">
-                    <div class="form-group">
-                      <label>Username</label>
-                      <input type="text" name="user" class="form-control" required>
-                    </div>
-                    <div class="form-group">
-                      <label>Email</label>
-                      <input type="email" name="email" class="form-control" required>
-                    </div>
-                    <div class="form-group">
-                      <label>Password</label>
-                      <input type="password" name="password" class="form-control" required>
-                    </div>
-                    <div class="form-group">
-                      <label>Confirm Password</label>
-                      <input type="password" name="confirm-password" class="form-control" required>
-                    </div>
-                    <button type="submit" name = "submit" class="btn btn-primary">Sign Up</button>
+                    <button type="submit" name = "submit" class="btn btn-class"> Login </button>
+                    <button type="submit" name = "submit" class="btn btn-class"> Signup </button>
                   </form>
                 </div>
 
       </div>
     </div>
-    </div>
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
   </body>
 </html>
