5f0bf7f2d70381039119cdd4d41c2dc6a998268e
1Noura71 <1norraa.s7@gmail.com>
2020-11-15 01:17:48 +0300

    Upadate styling link

--- include/header.php
+++ include/header.php
@@ -4,18 +4,10 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
-
-
     <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Landing</title>
-
     <link rel="stylesheet"  href="css/style.css">
-
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
-
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet"  href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
   </head>
   <body>
