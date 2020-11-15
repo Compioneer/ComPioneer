713a33be7f03ccb53f100fef8df7ce3139462187
1Noura71 <1norraa.s7@gmail.com>
2020-11-15 22:25:51 +0300

    Question cards

--- home.php
+++ home.php
@@ -18,11 +18,44 @@ require 'include\header.php';
     <a href="logout.php">Logout</a>
      </nav>
   </div>
-<a href="post_question.php">
-<button type="button" class="btn-class" id="btn-id">+</button>
-</a>
+  <section class= "box-scroll">
+  <div class="container-home">
+  <div class="card text-white bg-info mb-3">
+    <div class="card-header">username</div>
+    <div class="card-body">
+      <h5 class="card-title">Question Title </h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    </div>
+  </div>
+  <div class="card bg-light mb-3">
+    <div class="card-header">username</div>
+    <div class="card-body">
+      <h5 class="card-title">Question Title </h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    </div>
+  </div>
+  <div class="card text-white bg-dark mb-3">
+    <div class="card-header">username</div>
+    <div class="card-body">
+      <h5 class="card-title">Question Title </h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    </div>
+  </div>
+ </div>
+ </section>
 
-      <h2> Welcome </h2>
+ <section class="">
+
+ </section>
+
+
+
+
+
+
+    <a href="post_question.php">
+    <button type="button" class="btn-class" id="btn-id">+</button>
+    </a>
 
   </body>
 </html>
