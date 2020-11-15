fa9151314c77713f1af802d2a12d1d9b37ae10be
razanas <razan8022@gmil.com>
2020-11-15 19:06:09 +0300

    Initial navbar

--- home.php
+++ home.php
@@ -8,17 +8,21 @@ require 'include\header.php';
     <title> Welcome | Compioneer </title>
   </head>
   <body>
-
-<p>
-  testing  </p>
-
-      <a href="logout.php">Logout</a>
-
-      <p>hello 9</p>
+<div class="header">
+  <a href="#default" class="logo">
+     ComPioneer
+    </a>
+   <nav>
+    <a href="IT.php">IT</a>
+    <a href="CS.php">CS</a>
+    <a href="logout.php">Logout</a>
+     </nav>
+  </div>
 <a href="post_question.php">
 <button type="button" class="btn-class" id="btn-id">+</button>
 </a>
 
+      <h2> Welcome </h2>
 
   </body>
 </html>
