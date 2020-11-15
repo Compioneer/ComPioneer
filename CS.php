36cb28e03bdb571239e18f63d16ff7040b90e3b0
razanas <razan8022@gmil.com>
2020-11-15 19:07:37 +0300

    connecting new pages to navbar

---
+++ CS.php
@@ -0,0 +1,16 @@
+<?php
+    require 'include\header.php';
+?>
+<html lang="en" dir="ltr">
+  <body>
+<div class="header">
+  <a href="#default" class="logo">
+     ComPioneer
+    </a>
+   <nav>
+    <a href="IT.php">IT</a>
+    <a href="CS.php">CS</a>
+    <a href="logout.php">Logout</a>
+     </nav>
+  </div>
+</body>
