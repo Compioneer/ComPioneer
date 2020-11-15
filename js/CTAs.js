b0b8311700eb1646ae7cdc144b2011ca9e81b0a2
ghada-shl <ghada11pc@gmail.com>
2020-11-15 23:17:34 +0300

    login/signup javascript

--- js/CTAs.js
+++ js/CTAs.js
@@ -1,3 +1,19 @@
-function(){
 
-}
+document.addEventListener("DOMContentLoaded", () =>{
+const loginForm= document.querySelector("#login");
+const createAccountForm= document.querySelector("#createAccount");
+
+document.querySelector("#linkCreateAccount").addEventListener("click", e =>{
+e.preventDefault();
+loginForm.classList.add("form_hidden");
+createAccountForm.classList.remove("form_hidden");
+});
+
+document.querySelector("#linkLogin").addEventListener("click", e =>{
+
+e.preventDefault();
+loginForm.classList.remove("form_hidden");
+createAccountForm.classList.add("form_hidden");
+
+});
+});
