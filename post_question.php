973072efb09ab0847c80f116388b6fe130fa86e9
1Noura71 <1norraa.s7@gmail.com>
2020-11-17 15:02:18 +0300

    connect tag selector with database

--- post_question.php
+++ post_question.php
@@ -13,11 +13,20 @@
            <div class="form-group">
              <label for="exampleFormControlSelect2">Example multiple select</label>
              <select multiple class="form-control" id="exampleFormControlSelect2">
-               <option>1</option>
-               <option>2</option>
-               <option>3</option>
-               <option>4</option>
-               <option>5</option>
+               <?php
+                       $sql="SELECT * FROM tags";
+                       $show_tags_query=mysqli_query($con,$sql);
+                       if(!$show_tags_query){
+                           die("Erorr: ".mysqli_error($con));
+                       }else{
+                           $row_num=1;
+                           while($row=mysqli_fetch_assoc($show_tags_query)){
+                               $tag_title= $row['tag_title'];
+                               echo"<option>$tag_title</option>";
+                               $row_num++;
+                           }
+                       }
+               ?>
              </select>
            </div>
            <div class="form-group">
