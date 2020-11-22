3581f8321f996e0e5a545edfd42abfa5bb32c093
1Noura71 <1norraa.s7@gmail.com>
2020-11-22 11:17:28 +0300

    adding question query to home

--- home.php
+++ home.php
@@ -1,64 +1,95 @@
-
 <?php
 require 'include\header.php';
  ?>
- <!-- <li><a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-user-circle-o" aria-hidden="true"></i></a></li> -->
-           <!-- navgation  -->
-           <div class="home">
-           <script src="js/CTAs.js"></script>
-       <div class="headerin">
-          <a href="landing.php" class="logo">ComPioneer</a>
 
-              <div class="topnav" id="myTopnav">
-                <a href="FAQ.php">FAQ</a>
-                <div class="dropdown">
-                  <button class="dropbtn">
-                    <i class="fa fa-user-circle-o" aria-hidden="true"></i>
-                    <i class="fa fa-caret-down"></i>
-                  </button>
-                  <div class="dropdown-content">
-                    <a href="profile.php">My Profile</a>
-                    <a href="logout.php">Logout</a>
-                  </div>
-                </div>
-                <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
+          <!-- font awesome js link -->
+      <script src="https://use.fontawesome.com/5d66a18552.js"></script>
+    <div class="header">
+        <nav>
+        <a href="home.php" class="logo">ComPioneer</a>
+        <a class= "iconNav" href="profile.php"><i class="fa fa-user-o" aria-hidden="true"></i></a>
+        </nav>
+    </div>
+
+<!-- <div class="homeIllu"> -->
+
+
+
+    <!-- cards question section -->
+           <?php
+           session_start();
+       $sql= "SELECT * FROM questions";
+       $questions_query=mysqli_query($con, $sql);
+       if(!$questions_query){
+         die("Error: ".mysqli_error($con));
+       }else{
+         while($q_row=mysqli_fetch_assoc($questions_query)){
+           $q_id= $q_row['q_id'];
+           $q_username=$q_row['username'];
+          $q_timestamp = $q_row['timestamp'];
+           ?>
+           <div class="container-home">
+           <div class="card bg-light mb-3" id="coloring">
+             <div class="card-header">  <?php
+               echo $q_username;
+               ?></div>
+             <div class="card-body">
+               <h5 class="card-title"> <p><?php
+                echo $q_row['q_title'];
+                ?></p></h5>
+               <p class="card-text"><a href="answers.php?q_id=<?php echo $q_id; ?>"><?php echo $q_row['q_body']; ?></a></p>
+              <div class="timestamp">
+                <?php echo $q_timestamp;?>
               </div>
+              <a class="answer-link" href="answer.php"><i class="fas fa-comment"></i></a>
+
+             <!-- if(!($q_username===$_SESSION['username'])){
+                  echo "";
+              }else{
+                  echo "<a class='btn btn-info' href='include\update_question.php?q_id=$q_id'>EDIT</a>";
+              }
+            -->
+             </div>
 
 
-      </div>
+             <div class="col align-self-center">
+               <?php
+                 $sql_tags= "SELECT * FROM q_tags WHERE q_id= $q_id";
+                 $tag_id_query = mysqli_query($con, $sql_tags);
+                 if(!$tag_id_query){
+                   die("Error: ". mysqli_error($con));
+                 }else{
+                   while($tag_id_row= mysqli_fetch_assoc($tag_id_query)){
+                     $tag_id= $tag_id_row['tag_id'];
+                     $sql_tag_title= "SELECT * FROM tags WHERE tag_id= $tag_id";
+                     $tag_title_query= mysqli_query($con, $sql_tag_title);
 
-         <!-- cards question section -->
-      <section class= "box-scroll">
-        <div class="container-home">
-        <div class="card bg-light mb-3" id="coloring">
-          <div class="card-header">username</div>
-          <div class="card-body">
-            <h5 class="card-title">Question Title </h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          </div>
-        </div>
-        <div class="card bg-light mb-3">
-          <div class="card-header">username</div>
-          <div class="card-body">
-            <h5 class="card-title">Question Title </h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          </div>
-        </div>
-        <div class="card bg-light mb-3">
-          <div class="card-header">username</div>
-          <div class="card-body">
-            <h5 class="card-title">Question Title </h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          </div>
-       </div>
-       </div>
-    </section>
+                     if(!$tag_title_query){
+                       die("Error: ".mysqli_error($con));
+                     }else{
+                       while($tag_title_row=mysqli_fetch_assoc($tag_title_query)){
+                         $tag_title= $tag_title_row['tag_title'];
+                         ?> <span class="badge badge-pill badge-primary"><?php echo $tag_title; ?> </span> <?php
+                       }
+
+                     }
+                   }
+                 }
+
+               ?>
+             </div>
+             </div>
+           <?php
+         }
+       }
+     ?>
+
+      <a href="post_question.php">
+      <button type="button" class="btn-class" id="btn-id">+</button>
+      </a>
+      <!-- </div> -->
 
 
 
-        <a href="post_question.php">
-        <button type="button" class="btn-class" id="btn-id">+</button>
-        </a>
-</di>
   </body>
 </html>
