14303870e8aefe7bb4030b37418d6713739a363b
ghada-shl <ghada11pc@gmail.com>
2020-11-27 18:00:27 +0300

    form validation javascript

--- js/CTAs.js
+++ js/CTAs.js
@@ -26,3 +26,35 @@ function myFunction() {
   }
 }
 $('select').selectpicker();
+
+
+// form validation
+
+function isEmail(email){
+return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
+function validateform(){
+var name=document.signform.user.value;
+var password=document.signform.password.value;
+var email=document.signform.email.value;
+var conpass=document.signform.conpass.value;
+
+if (name.length<6){
+  alert("Name must be at least 6 characters long ");
+  return false;
+}else if(password.length<6){
+  alert("Password must be at least 6 characters long");
+  return false;
+  }
+else if(password !== conpass){
+  alert("Password mismatch");
+  return false;
+  }
+  
+  else if(!isEmail(email)){
+    alert("Invalid Email");
+    return false;
+    }
+
+}
