d9309ee02baf6db698fdcd62fc299f254d2e3e23
ghada-shl <ghada11pc@gmail.com>
2020-11-27 18:09:08 +0300

    landing finishup/form validation

--- landing.php
+++ landing.php
@@ -1,21 +1,24 @@
+
 <?php
   require 'include\header.php';
 ?>
 
+
+
 <div class="header">
   <a href="home.php" class="logo">
-    ComPioneer
+      ComPioneer
   </a>
 </div>
 
    <div class="container">
       <div class="row">
             <div class="col-md-7 mb-3">
-                <img src="images/download-icons88.png" class="img-fluid" alt="illustration">
+                <img src="images/newillu2.png" class="img-fluid" alt="illustration">
             </div>
 
                <div class="col-md-4">
-                 <h4 class="signin-text mb-3"> Ask a Question,</br> Get an Answer</h4>
+                 <h4 class="signin-text mb-3"> The local COC students Platform </h4>
                  <h3> </br> </h3>
 
 
@@ -31,39 +34,36 @@
                                      <button type="submit" name = "submit" class="btn btn-class" id="loginbtn"> Login </button>
 
                                      <p class="form_text">
-                                       <a class="form_link" id="linkCreateAccount">  </br> Don't have an Account? Create Account</a>
+                                       <a class="form_link" id="linkCreateAccount">  </br>Don't have an Account? Create Account</a>
                                      </p>
                                    </form>
+<!-- <div id="error"></div> -->
+               <form action="signup.php" method="post" id="createAccount" class="form_hidden" name="signform" onsubmit="return validateform()">
 
-
-               <form action="signup.php" method="post" id="createAccount" class="form_hidden">
-
-           <div class="form-group">
+       <div class="form-group ">
                      <label>Username</label>
-                     <input type="text" name="user" class="form-control" required>
+                     <input type="text" name="user" id="signuser"class="form-control" required>
                    </div>
 
-                  <div class="form-group">
+                  <div class="form-group ">
                      <label>Email</label>
-                     <input type="email" name="email" class="form-control" required>
+                     <input type="email" name="email" id="signemail" class="form-control">
                    </div>
 
                   <div class="form-group">
                     <label>Password</label>
-                    <input type="password" name="password" class="form-control" required>
+                    <input type="password" name="password" id="signpass" class="form-control" >
+
                   </div>
 
                   <div class="form-group">
-                    <input type="password" name="confirm-password" class="form-control" id="confirmPass" autofocus placeholder="Confirm password" required>
+                    <input type="password" name="conpass" class="form-control" id="signconpass" autofocus placeholder="Confirm password">
                   </div>
-
-
-
 
                   <button type="submit" name = "submit" class="btn btn-class" id="signupBtn"> Sign up </button>
 
                    <p class="form_text">
-                    <a class="form_link" id="linkLogin"> </br> Already have an Account? Sign in</a>
+                    <a class="form_link" id="linkLogin"> </br>Already have an Account? Sign in</a>
                   </p>
              </form>
 
@@ -77,7 +77,7 @@
 
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-
 <script src="js/CTAs.js"></script>
+
   </body>
 </html>
